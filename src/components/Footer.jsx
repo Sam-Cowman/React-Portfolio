1fb050b9fd87e9fbd8b9e08e503fa633e0c648a4
Sam-Cowman <sam.p.cowman@gmail.com>
2024-07-23 15:45:33 -0500

    removed stack overflow

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,7 @@ function Footer() {
             <h4>&copy; 2024 SPC Portfolio</h4>
             <p>
                 <a href="https://github.com/Sam-Cowman">GitHub</a> | 
-                <a href="https://www.linkedin.com/in/samantha-cowman-07695364/">LinkedIn</a> | 
-                <a href="https://stackoverflow.com/users/your-stackoverflow-profile">Stack Overflow</a>
+                <a href="https://www.linkedin.com/in/samantha-cowman-07695364/">LinkedIn</a> 
             </p>
         </footer>
     );
