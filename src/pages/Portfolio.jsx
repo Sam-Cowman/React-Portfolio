7929edf5b5cf4829a7c9deed31adde5c4e5b65bd
Sam-Cowman <sam.p.cowman@gmail.com>
2024-07-24 13:47:01 -0500

    moved images into public folder

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -7,25 +7,25 @@ function Portfolio() {
     const projects = [
         {
             title: 'SEO Marketing',
-            image: '/src/assets/images/Marketing src .jpg',
+            image: '/assets/images/Marketing src .jpg',
             deployedLink: 'https://sam-cowman.github.io/SEO-Marketing-Site/',
             githubLink: 'https://github.com/sam-cowman/SEO-Marketing-Site'
         },
         {
             title: 'Weather Dashboard',
-            image: '/src/assets/images/weather-dashboard.jpg',
+            image: '/assets/images/weather-dashboard.jpg',
             deployedLink: 'https://drive.google.com/file/d/1teiglVdim21wCeRCSmOlVX0vLycf-Ij5/view',
             githubLink: 'https://github.com/Sam-Cowman/Weather-Dashboard'
         },
         {
             title: 'E-Commerce Site',
-            image: '/src/assets/images/e-commerce-screenshot.jpg', 
+            image: '/assets/images/e-commerce-screenshot.jpg', 
             deployedLink: 'https://github.com/Sam-Cowman/E-Commerce',
             githubLink: 'https://github.com/Sam-Cowman/E-Commerce'
         },
         {
             title: 'Tech Blog',
-            image: '/src/assets/images/blog-homepage.jpg',
+            image: '/assets/images/blog-homepage.jpg',
             deployedLink: 'https://tech-blog-9kmk.onrender.com',
             githubLink: 'https://github.com/Sam-Cowman/Tech-Blog'
         }
